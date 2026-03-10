677959712ddcd5c4cbc7424d3bdb5b9ea95fa0e1
siluk00 <sil-uk@hotmail.com>
2026-03-10 16:47:36 -0300

    inverted index

--- cli/invertedIndex.py
+++ cli/invertedIndex.py
@@ -1,30 +1,39 @@
-import json, string
+import json, string, os, pickle
 from nltk.stem import PorterStemmer
-from keyword_search_cli import tokenize_input
+
+def tokenize_input(phrase, stopwords, table, stemmer): #returns a list of tokenized inputs
+    tokens = phrase.lower().translate(table).split(" ") #uncapitalize and remove punctuation to list
+    tokens = list(filter(lambda x: x != "",tokens)) #removes blank
+    tokens = list(filter(lambda x: x not in stopwords, tokens)) #removes words without meaning
+    tokens = list(map(lambda x: stemmer.stem(x),   tokens)) #turns words to their stem
+    return tokens 
 
 class InvertedIndex:
-    def __init__(self, index, docmap):
-        self.index = dict()
-        self.docmap = dict()
+    def __init__(self):
+        self.index = dict() #a dictionary mappin words to id / inverted index
+        self.docmap = dict() # a dictionary mapping id to  title + description
     
+    #adds text to the set with doc_id key
     def _add_document(self, doc_id, text):
-        if text in self.index:
-            self.index[text].add(doc_id)
-        else:
+        if not (text in self.index):
             self.index[text] = set()
+        self.index[text].add(doc_id)
 
+    # gets the set of doc_ids as  list for the term searched
     def get_document(self, term):
+        if term not in self.index:
+            return []
+        
         document = []
-        for elem in self.index[term]:
-            document.append(elem)
+        document.extend(self.index[term]) #extend transforms set into list
         document.sort()
-        return document
+        return document #returns a list
     
+    #builds the inverted index 
     def build(self):
         dict = {}
         with open("data/movies.json", 'r') as f:
                 dict = json.load(f)
-        results = []
     
         stopwords = []
         with open("data/stopwords.txt", 'r') as f:
@@ -39,7 +48,28 @@ class InvertedIndex:
             self.docmap[entry["id"]] = entry
 
             for token in tokens:
-                self._add_document(token, entry["id"])
+                self._add_document(entry["id"], token)
+    
+    def save(self):
+        index_path = "cache/index.pkl"
+        docmap_path = "cache/docmap.pkl"
+        os.makedirs(os.path.dirname(index_path), exist_ok=True)
+
+        with open(index_path, 'wb') as f:
+            pickle.dump(self.index, f)
+        
+        with open(docmap_path, 'wb') as f:
+            pickle.dump(self.docmap, f)
+
+    def load(self):
+        index_path = "cache/index.pkl"
+        docmap_path = "cache/docmap.pkl"
+        with open(index_path, 'rb') as f:
+            self.index = pickle.load(f)
+        with open(docmap_path, 'rb') as f:
+            self.docmap = pickle.load(f)
+       
+        
 
             
              
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,10 +2,11 @@
 
 import argparse, json, string
 from nltk.stem import PorterStemmer
+from invertedIndex import InvertedIndex
 
 
-def tokenize_input(phrase, stopwords, table, stemmer):
-    tokens = phrase.lower().translate(table).split(" ") #uncapitalize and remove punctuation to list
+def tokenize_input(word, stopwords, table, stemmer):
+    tokens = word.lower().translate(table).split(" ") #uncapitalize and remove punctuation to list
     tokens = list(filter(lambda x: x != "",tokens)) #removes blank
     tokens = list(filter(lambda x: x not in stopwords, tokens)) #removes words without meaning
     tokens = list(map(lambda x: stemmer.stem(x),   tokens)) #turns words to their stem
@@ -34,24 +35,54 @@ def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands") # subcomnmands like git add
 
+    #search parser
     search_parser = subparsers.add_parser("search", help="Search movies using BM25") #name + search + query
     search_parser.add_argument("query", type=str, help="Search query")
 
+    #build parser
+    subparsers.add_parser("build", help="Builds the index")
+
     args = parser.parse_args()
+
+    inverted_index = InvertedIndex()
 
     match args.command:
         case "search":
+            try:
+                inverted_index.load()
+            except Exception as e:
+                print("cannot find file")
+                exit(1)
+            
             dict = {}
-            with open("data/movies.json", 'r') as f:
-                dict = json.load(f)
 
-            results = tokenize_text(dict, args)
+            results = []
+    
+            stopwords = []
+            with open("data/stopwords.txt", 'r') as f:
+                txt = f.read()
+                stopwords = txt.splitlines()
+    
+            stemmer = PorterStemmer()
+            table = str.maketrans("", "", string.punctuation) #table of transformation, puntuation -> ""
+            input = tokenize_input(args.query, stopwords, table, stemmer)
+            results = []
+            for element in input:
+                results.extend(inverted_index.get_document(element))
+
 
             print(f"Searching for: {args.query}")
             for i in range(len(results)):
                 if i == 5:
                     break
-                print(f"{i+1}. {results[i]["title"]}")
+                print(f"{i+1}. {inverted_index.docmap[results[i]]['title']}") 
+        case "build":
+            inverted_index.build()
+            inverted_index.save()
+            #print(inverted_index.index)
+            docs = inverted_index.get_document("merida")
+            print(f"First document for token 'merida' = {docs[0]}")
+
         case _:
             parser.print_help()
 

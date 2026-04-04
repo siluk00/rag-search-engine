d989a29c82c8b2be2e2d2b074fa03ef7195a1315
siluk00 <sil-uk@hotmail.com>
2026-04-04 13:01:40 -0300

    multimodal search

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from sentence_transformers import SentenceTransformer
+from lib.semantic_search import cosine_similarity
 
 def verify_image_embedding(path):
     multimodal_search = MultimodalSearch()
@@ -7,9 +8,33 @@ def verify_image_embedding(path):
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
 
 class MultimodalSearch():
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, doclist, model_name="clip-ViT-B-32"):
         self.model = SentenceTransformer(model_name, device='cpu')
+        self.doclist = doclist
+        self.texts = [f"{doc['title']}: {doc['description']}" for doc in doclist]
+        self.text_embeddings = self.model.encode(self.texts)
 
     def embed_image(self, path):
         img = Image.open("data/paddington.jpeg")
         return self.model.encode(img)
+
+    def search_with_image(self , imgPath):
+        img = Image.open("data/paddington.jpeg")
+        imgEmbedding = self.model.encode(img)
+        similarity = []
+        
+        for embedding in self.text_embeddings:
+            similarity.append(cosine_similarity(embedding, imgEmbedding))
+        
+        doc_score_pairs = list(zip(self.doclist, similarity))
+        doc_score_pairs.sort(key=lambda x: x[1], reverse=True)
+        top_5_with_scores = []
+        for doc, score in doc_score_pairs[:5]:
+            top_5_with_scores.append({
+                "id": doc.get("id"),
+                "title": doc.get("title"),
+                "description": doc.get("description"),
+                "similarity": float(score)  # Ensure it's a standard float for JSON compatibility
+            })
+
+        return top_5_with_scores
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,5 +1,9 @@
-import argparse
-from lib.multimodal_search import verify_image_embedding
+import argparse, json
+from lib.multimodal_search import verify_image_embedding, MultimodalSearch
+
+def load_movies():
+    with open('data/movies.json', 'r') as f:
+        return json.load(f)['movies']
 
 def main():
     parser = argparse.ArgumentParser(description="Retrieval Augmented Generation CLI")
@@ -8,12 +12,24 @@ def main():
     embed_parser = subparsers.add_parser("verify_image_embedding", help="Perform image embedding from path")
     embed_parser.add_argument("path", type=str, help="Path of image")
 
+    img_search_parser = subparsers.add_parser("image_search", help="Perform image search")
+    img_search_parser.add_argument("path", type=str, help="Path of image")
+
     args = parser.parse_args()
 
     match args.command:
         case "verify_image_embedding":
             print("hello")
             verify_image_embedding(args.path)
+        case "image_search":
+            docs = load_movies()
+            multi_modal_search = MultimodalSearch(docs)
+            results = multi_modal_search.search_with_image(args.path)
+            i = 1
+            for result in results:
+                print(f"{i}. {result["title"]} (similarity: {result["similarity"]:.3f})")
+                print(f"   {result["description"]}")
+                i += 1
         case _:
             parser.print_help()
 
